936e83458c8eebfb2b51aa3117b557a441761af4
alejoevilches <alejoevilches@gmail.com>
2023-12-03 13:29:04 -0300

    render list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 export function App(){
+    const [gifts, setGifts]=useState(["Campera de River", "Camiseta de River", "Pantalón de River"]);
+    return (
+        <main>
+            <h1>Regalos</h1>
+            <ul>
+                {gifts.map(el=>{
+                    return(
+                        <li key={el}>{el}</li>
+                    )
+                })}
+            </ul>
+        </main>
+    )
 }
