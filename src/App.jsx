f30198a7da80043b6e134963f60a571b43291621
alejoevilches <alejoevilches@gmail.com>
2023-12-03 13:24:53 -0300

    starting day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
-import { useState } from "react"
 import "./App.css";
 
 export function App(){
-    const [gifts, setGifts]=useState(["Remera de River", "Campera de River", "Gorra de River"]);
-    return(
-        <main>
-            <div className="giftsContainer">
-                <h1>Regalos</h1>
-                <ul>
-                    {gifts.map(el=>{
-                        return(
-                            <li key={el}>
-                                <p>{el}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </main>
-    )
 }
