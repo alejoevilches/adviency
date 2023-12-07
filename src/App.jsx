3fc832227ffa1a288fa6b19f0296f5c7c24bdc56
alejoevilches <alejoevilches@gmail.com>
2023-12-07 00:15:48 -0300

    start welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import "./App.css";
 import { IconTrash } from "@tabler/icons-react";
+import { useGifts } from "./hooks/useGifts";
 
 export function App(){
+    const {gifts, addGift, deleteGift, deleteAllGifts}=useGifts();
+    const handleAddButton=(e)=>{
+        e.preventDefault();
+        const gift=e.target[0].value;
+        addGift(gift);
+    }
     return (
         <main>
             <section className="giftsContainer">
                 <h1>Regalos</h1>
+                <ul>
+                    {gifts.map((el)=>{
+                        return( 
+                        <article key={el}>
+                            <li>
+                                {el}
+                            </li>
+                            <button className="deleteButton" onClick={()=>deleteGift(el)}><IconTrash /></button>
+                        </article>
+                        )
+                    })}
+                </ul>
+                <form onSubmit={handleAddButton}>
+                    <input type="text" placeholder="Ingresá tu regalo" />
+                    <button className="button">
+                        <span className="button-content">Agregar</span>
+                    </button>
+                </form>
+                {gifts.length > 0 && 
+                    <button className="button deleteAllButton" onClick={deleteAllGifts}>
+                        <span className="button-content">Eliminar todo</span>
+                    </button>
+                }
             </section>
         </main>
     )
---
+++ src/hooks/useGifts.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export const useGifts=()=>{
+    const [gifts, setGifts]=useState(["Hola", "Regalo"])
+
+    const addGift=(el)=>{
+        const newGiftList=[...gifts, el];
+        setGifts(newGiftList);
+    }
+
+    const deleteGift=(el)=>{
+        const newGiftList=[...gifts]
+        const i=newGiftList.indexOf(el);
+        newGiftList.splice(i,1);
+        setGifts(newGiftList);
+    }
+
+    const deleteAllGifts=()=>{
+        setGifts([])
+    }
+
+    return {gifts, addGift, deleteGift, deleteAllGifts}
+}
