cbebb29e055e93cdb50ae52889a5d737772adeca
alejoevilches <alejoevilches@gmail.com>
2023-12-07 21:30:11 -0300

    work on useGifts hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import { IconTrash } from "@tabler/icons-react";
 import "./App.css";
 import { useGifts } from "./hooks/useGifts";
 
+function DeleteAllButton(){
+    return (
+        <button className="button deleteAllButton">
+            <span>Eliminar todos</span>
+        </button>
+    )
+}
+
 export function App(){
-    const {gifts}=useGifts();
+    const {gifts, addGift}=useGifts();
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const gift=e.target[0].value;
+        addGift(gift);
+        e.target[0].value=""
+    }
     return (
         <main>
             <section className="giftsContainer">
@@ -20,12 +34,13 @@ export function App(){
                         )
                     })}
                 </ul>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Ingresá tu regalo" />
                     <button className="button">
                         <span className="button-content">Agregar</span>
                     </button>
                 </form>
+                {gifts.length > 0 && <DeleteAllButton />}
             </section>
         </main>
     )
--- src/hooks/useGifts.jsx
+++ src/hooks/useGifts.jsx
@@ -2,6 +2,22 @@ import { useState } from "react"
 
 export const useGifts=()=>{
     const [gifts, setGifts]=useState(["Hola"]);
+
+    const addGift=(el)=>{
+        const newGiftList=[...gifts, el];
+        setGifts(newGiftList);
+    }
+
+    const deleteGift=(el)=>{
+        const newGiftList=structuredClone(gifts);
+        const i=newGiftList.indexOf(el);
+        newGiftList.splice(i,1);
+        setGifts(newGiftList);
+    }
+
+    const deleteAllGifts=()=>{
+        setGifts([]);
+    }
     
-    return {gifts}
+    return {gifts, addGift, deleteGift, deleteAllGifts}
 }
