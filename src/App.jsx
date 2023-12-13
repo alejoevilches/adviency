e1cbe8059f67eefb87bdaf7581a7f76d34c2f11a
alejoevilches <alejoevilches@gmail.com>
2023-12-13 17:09:47 -0300

    start day 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,6 @@
 import { IconTrash } from "@tabler/icons-react";
 import "./App.css";
-import { useGifts } from "./hooks/useGifts";
-import { useModals } from "./hooks/useModals";
-import { Modal } from "./components/Modal";
-
 
 export function App(){
-    const {gifts, addToGifts, deleteGift, deleteAllGifts}=useGifts();
-    const {modals, setModals}=useModals();
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        const gift={
-            name:e.target[0].value,
-            qty:parseInt(e.target[1].value)
-        }
-        if (gift.name === "") {
-            return setModals({ type: "emptyGift" });
-        }
-        addToGifts(gift);
-        e.target[0].value="";
-    }
-    return (
-        <main>
-            <section className="giftsContainer">
-                <h1>Regalos</h1>
-                <ul>
-                    {gifts.map(el=>{
-                        return (
-                        <article key={el.name}>
-                            <li>
-                                <p>{el.name}</p><span> Cantidad: {el.qty}</span>
-                            </li>
-                            <button className="deleteButton" onClick={(el)=>deleteGift(el)}><IconTrash /></button>
-                        </article>
-                        )
-                    })}
-                </ul>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Ingresá tu regalo" />
-                    <input type="number" name="qty" id="qty" min={1} defaultValue={1}/>
-                    <button className="button">
-                        <span className="button-content">Agregar</span>
-                    </button>
-                </form>
-                {gifts.length==0 &&
-                    <p className="message">No seas Grinch y agregate un regalo! 🎄</p>
-                }
-                {gifts.length>0 &&
-                    <button className="deleteAllButton button" onClick={deleteAllGifts}>
-                        <span className="button-content">Eliminar todos</span>
-                    </button>
-                }
-            </section>
-            {modals.type=="emptyGift" && 
-                <Modal>
-                    <h1>No podes agregar regalos vacios en la lista. Papa Noel no va a entender que traerte!</h1>
-                    <button className="button" onClick={()=>setModals({type:null})}>
-                        <span className="button-content">Aceptar</span>
-                    </button>
-                </Modal>
-            }
-        </main>
-    )
+
 }
--- src/hooks/useModals.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react"
-
-export const useModals=()=>{
-    const [modals, setModals]=useState({type: null})
-
-    return {modals, setModals}
-}
