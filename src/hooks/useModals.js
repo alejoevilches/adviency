3960a773344074f4d5d2a3a54e613fd817d828bd
alejoevilches <alejoevilches@gmail.com>
2023-12-15 18:04:31 -0300

    start day 10

--- src/hooks/useModals.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-export const useModals=()=>{
-    const [modals, setModals]=useState({type:null})
-    const toggle=()=>{
-        setModals({type:null})
-    };
-    return {modals, setModals, toggle}
-}
