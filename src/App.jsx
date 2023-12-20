44e2b85edb61c4b221be7eada4a03f42a570fff1
alejoevilches <alejoevilches@gmail.com>
2023-12-20 16:42:15 -0300

    starting day 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
 import "./App.css";
-import { useGiftsStore } from "./store/useGiftsStore";
-import { Chip } from "./components/Chip";
-import { useEffect, useState } from "react";
-import { Form } from "./components/Form";
 
 export function App(){
-   const {gifts, deleteAllGifts}=useGiftsStore() 
-   const [addGiftModal, setAddGiftModal]=useState(false);
-   const closeModal=()=>{
-    setAddGiftModal(false);
-   }
-
-   useEffect(()=>{
-    localStorage.setItem("gifts", JSON.stringify(gifts))
-   }, [gifts])
-
-   return (
-    <main>
-        <section className="giftsContainer">
-            <h1>Regalos</h1>
-            <button className="button" onClick={()=>setAddGiftModal(true)}>
-                <span className="button-content">Agregar regalos</span>
-            </button>
-            <ul>
-                {gifts.map(el=>{
-                    return (
-                        <Chip key={el.name} el={el} />
-                    )
-                })}
-            </ul>
-        {gifts.length>0 &&
-            <button className="deleteAllButton button" onClick={deleteAllGifts}>
-                <span className="button-content">Eliminar todo</span>
-            </button>
-        }
-        {gifts.length==0 &&
-            <p className="message">No seas Grinch y agregate un regalo! 🎄</p>
-        }
-        </section>
-        {addGiftModal && 
-            <Form closeModal={closeModal} />
-        }
-    </main>
-   )
+    const {gifts}=useGiftsStore();
+    return (
+        <main>
+            <section className="giftsContainer">
+                <h1>Regalos</h1>
+                <button className="button">
+                    <span className="button-content">Agregar regalo</span>
+                </button>
+                <ul>
+                    {}
+                </ul>
+            </section>
+        </main>
+    )
 }
--- src/store/useGiftsStore.js
+++ src/store/useGiftsStore.js
@@ -3,43 +3,41 @@ import { create } from "zustand";
 export const useGiftsStore=create((set)=>{
     const getGiftsFromLocalStorage=()=>{
         const storedGifts=localStorage.getItem("gifts");
-        return storedGifts
+        return storedGifts 
             ? JSON.parse(storedGifts)
             : [];
     }
 
     const gifts=getGiftsFromLocalStorage();
-    console.log(gifts);
 
     const addToGifts=(el)=>{
         set((state)=>{
             const {gifts}=state;
-            const i=gifts.findIndex(gift=>gift.name==el.name)
+            const i=gifts.findIndex(gift=>gift.name==el.name);
             if (i!=-1){
                 const newGiftList=structuredClone(gifts);
                 newGiftList[i].qty+=el.qty;
-                return {gifts:newGiftList};
+                return {gifts:newGiftList}
             }
             return {gifts:[...gifts, el]}
         })
     }
 
-    const deleteGift=(el)=>{
+    const deleteToGifts=(el)=>{
         set((state)=>{
             const {gifts}=state;
             const i=gifts.findIndex(gift=>gift.name==el.name);
             const newGiftList=structuredClone(gifts);
-            console.log(i);
             newGiftList.splice(i,1);
-            return{gifts:newGiftList}
+            return {gifts:newGiftList};
         })
     }
 
     const deleteAllGifts=()=>{
-        set({
-            gifts:[]
+        set(()=>{
+            return {gifts:[]}
         })
     }
 
-    return {gifts, addToGifts, deleteAllGifts, deleteGift};
+    return {gifts, addToGifts, deleteToGifts, deleteAllGifts};
 })
